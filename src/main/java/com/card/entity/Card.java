8b0f8f7eda84a7b76b3932c951c2c6697cd1f606
Moayad Faraj <moayadalfarga.20@gmail.com>
2026-03-10 16:19:27 +0300

    add test unit and docker file

--- src/main/java/com/card/entity/Card.java
+++ src/main/java/com/card/entity/Card.java
@@ -1,14 +1,8 @@
 package com.card.entity;
 
-import com.card.enums.CardStatus;
 import com.card.enums.CardType;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "cards")
